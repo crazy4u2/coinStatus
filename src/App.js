6c8ccb3a597490903c16e2e391e2b62a2f9a69bd
jacob you <jejaforce@gmail.com>
2021-05-20 13:58:17 +0900

    404 처리 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Bookmark from "./Bookmark";
 
 function App() {
   return (
     <div>
-      <Route path="/" component={Home} exact />
-      <Route path="/bookmark" component={Bookmark} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/bookmark" component={Bookmark} />
+        <Route
+          render={({ location }) => (
+            <div className="no-page">없는 페이지입니다.</div>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
