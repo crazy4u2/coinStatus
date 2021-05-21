a25fddc5243be4d3ffc7d51ac3fb5504034eafba
jacob you <jejaforce@gmail.com>
2021-05-22 00:04:39 +0900

    코인상세에 대한 페이지 설정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Bookmark from "./Bookmark";
+import Details from "./Details";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/bookmark" component={Bookmark} />
+        <Route path="/details/:coin" component={Details} />
         <Route
           render={({ location }) => (
             <div className="no-page">없는 페이지입니다.</div>
